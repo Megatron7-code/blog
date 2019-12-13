1447ba8e98985829b4244a123fdb8d42325cde72
megatron <megatron@megatrondeiMac.local>
2019-12-13 12:01:39 +0800

    adjust

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,6 @@ echo 'blog.vm321.com' > CNAME
 git init && git add -A && git commit -m 'deploy'
 
 git push -f git@github.com:Megatron7-code/blog.git master:gh-pages
+git push -f git@gitee.com:megatron7/blog.git master:gh-pages
 
 cd -
