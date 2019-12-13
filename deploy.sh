6c3e82b7e1f051a16dde99d65316466aba6e8726
megatron <megatron@megatrondeiMac.local>
2019-12-13 15:09:23 +0800

    adjust

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,6 @@ git init && git add -A && git commit -m 'deploy'
 
 git push -f git@github.com:Megatron7-code/blog.git master:gh-pages
 git push -f git@gitee.com:megatron7/blog.git master:gh-pages
+git push -f git@git.coding.net:megatron7/blog.git master:coding-pages
 
 cd -
