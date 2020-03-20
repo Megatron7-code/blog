e5aa63c8300b2c53605c9f3fbec7e2231dcb2291
megatron <megatron@megatrondeiMac.local>
2020-03-20 10:50:37 +0800

    adjust

--- docs/.vuepress/theme/components/home.vue
+++ docs/.vuepress/theme/components/home.vue
@@ -49,28 +49,6 @@
   </main>
 </template>
 <script>
-export default {
-  name: "App",
-  mounted() {
-    const script = document.createElement("script");
-    script.src =
-      "https://s9.cnzz.com/z_stat.php?id=1278699800";
-    script.language = "JavaScript";
-    document.body.appendChild(script);
-  },
-  watch: {
-    $route() {
-      if (window._czc) {
-        let location = window.location;
-        let contentUrl = location.pathname + location.hash;
-        let refererUrl = "/";
-        window._czc.push(["_trackPageview", contentUrl, refererUrl]);
-      }
-    }
-  }
-}
-</script>
-<script>
 import NavLink from '@theme/components/NavLink.vue'
 
 export default {
