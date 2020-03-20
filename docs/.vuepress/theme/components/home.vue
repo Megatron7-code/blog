ea6bd802d9aecec5fb6ae55c35d1361a04b75809
megatron <megatron@megatrondeiMac.local>
2020-03-20 11:31:08 +0800

    adjust

--- docs/.vuepress/theme/components/home.vue
+++ docs/.vuepress/theme/components/home.vue
@@ -64,31 +64,7 @@ export default {
         text: this.data.actionText
       }
     }
-  },
-  mounted() {
-       // 创建cnzz统计js
-      const script = document.createElement('script')
-      script.src = 'https://s9.cnzz.com/z_stat.php?id=1278699800'
-      script.language = 'JavaScript'
-      document.body.appendChild(script)
-    },
-    watch: {
-      '$route': {
-        handler(to, from) {
-          setTimeout(() => { //避免首次获取不到window._czc
-            if (window._czc) {
-              let location = window.location;
-              let contentUrl = location.pathname + location.hash;
-              let refererUrl = '/';
-              // 用于发送某个URL的PV统计请求，
-              window._czc.push(['_trackPageview', contentUrl, refererUrl])
-              window._czc.push(["_setAutoPageview", false]);
-            }
-          }, 300)
-        },
-        immediate: true  // 首次进入页面即执行
-      }
-    }
+  }
 }
 </script>
 
